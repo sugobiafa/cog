sugobiafa/cog

commit 294a9199f936a368379b3d839f7fd1d500f30847

Run setup on a thread
Currently, Runner.setup() blocks. When we call it in the FastAPI app
"startup" hook, we block the initialisation of the HTTP server itself.

That in turn means that any attempts to connect to the HTTP interface
while setup is running are refused, so we cannot successfully shut down
a model container while setup is running. This leaves model pods in a
broken state for up to 30m (the K8S termination grace period) until they
are killed.

This changes Runner.setup() to a non-blocking method that returns an
AsyncResult. We can then poll this object when the healthcheck endpoint
is called to find out whether setup is complete.

Note that it is not a requirement to call the healthcheck endpoint.
While setup is running, prediction endpoints will return a 409 Conflict
indicating that the predictor is busy, and will start accepting requests
once setup has completed. (Although note that currently, they will still
accept requests even if setup has failed.)

Signed-off-by: Nick Stenning <nick@whiteink.com>

python/cog/server/http.py
@@ -58,6 +58,7 @@ def create_app(
 
     app.state.health = Health.STARTING
     app.state.setup_result = None
+    app.state.setup_result_payload = None
 
     runner = PredictionRunner(
         predictor_ref=predictor_ref,
@@ -80,21 +81,7 @@ def create_app(
         # https://github.com/tiangolo/fastapi/issues/4221
         RunVar("_default_thread_limiter").set(CapacityLimiter(threads))  # type: ignore
 
-        setup_start = datetime.now(timezone.utc)
-        setup_status, setup_logs = runner.setup()
-        setup_complete = datetime.now(timezone.utc)
-
-        app.state.setup_result = {
-            "logs": setup_logs,
-            "status": setup_status,
-            "started_at": setup_start,
-            "completed_at": setup_complete,
-        }
-
-        if setup_status == schema.Status.SUCCEEDED:
-            app.state.health = Health.READY
-        else:
-            app.state.health = Health.SETUP_FAILED
+        app.state.setup_result = runner.setup()
 
         probes = ProbeHelper()
         probes.ready()
@@ -113,6 +100,7 @@ def create_app(
 
     @app.get("/health-check")
     def healthcheck() -> Any:
+        _check_setup_result()
         if app.state.health == Health.READY:
             health = Health.BUSY if runner.is_busy() else Health.READY
         else:
@@ -120,7 +108,7 @@ def create_app(
         return jsonable_encoder(
             {
                 "status": health.name,
-                "setup": app.state.setup_result,
+                "setup": app.state.setup_result_payload,
             }
         )
 
@@ -240,6 +228,25 @@ def create_app(
         log.info("shutdown requested via http")
         shutdown_event.set()
         return JSONResponse({}, status_code=200)
+
+    def _check_setup_result() -> Any:
+        if app.state.setup_result is None:
+            return
+
+        if not app.state.setup_result.ready():
+            return
+
+        result = app.state.setup_result.get()
+
+        if result["status"] == schema.Status.SUCCEEDED:
+            app.state.health = Health.READY
+        else:
+            app.state.health = Health.SETUP_FAILED
+
+        app.state.setup_result_payload = result
+
+        # Reset app.state.setup_result so future calls are a no-op
+        app.state.setup_result = None
 
     return app
 

python/cog/server/runner.py
@@ -54,24 +54,26 @@ class PredictionRunner:
         self._shutdown_event = shutdown_event
         self._upload_url = upload_url
 
-    def setup(self) -> Tuple[schema.Status, str]:
-        _logs = []
-        _status = None
+    def setup(self) -> AsyncResult:
+        if self.is_busy():
+            raise RunnerBusyError()
 
-        try:
-            for event in self._worker.setup():
-                if isinstance(event, Log):
-                    _logs.append(event.message)
-                elif isinstance(event, Done):
-                    _status = (
-                        schema.Status.FAILED if event.error else schema.Status.SUCCEEDED
-                    )
-        except Exception:
-            _status = schema.Status.FAILED
+        def handle_error(error: BaseException) -> None:
+            # Re-raise the exception in order to more easily capture exc_info,
+            # and then trigger shutdown, as we have no easy way to resume
+            # worker state if an exception was thrown.
+            try:
+                raise error
+            except Exception:
+                log.error("caught exception while running setup", exc_info=True)
+                self._shutdown_event.set()
 
-        assert _status is not None, "must receive done event from setup"
-
-        return (_status, "".join(_logs))
+        self._result = self._threadpool.apply_async(
+            func=setup,
+            kwds={"worker": self._worker},
+            error_callback=handle_error,
+        )
+        return self._result
 
     # TODO: Make the return type AsyncResult[schema.PredictionResponse] when we
     # no longer have to support Python 3.8
@@ -80,7 +82,10 @@ class PredictionRunner:
     ) -> Tuple[schema.PredictionResponse, AsyncResult]:
         # It's the caller's responsibility to not call us if we're busy.
         if self.is_busy():
-            assert self._response is not None
+            # If self._result is set, but self._response is not, we're still
+            # doing setup.
+            if self._response is None:
+                raise RunnerBusyError()
             assert self._result is not None
             if prediction.id is not None and prediction.id == self._response.id:
                 return (self._response, self._result)
@@ -274,6 +279,37 @@ class PredictionEventHandler:
             # The re-raised exception will be caught and cause the prediction
             # to be failed, with a useful error message.
             raise FileUploadError("Got error trying to upload output files") from error
+
+
+def setup(*, worker: Worker):
+    logs = []
+    status = None
+    started_at = datetime.now(tz=timezone.utc)
+
+    try:
+        for event in worker.setup():
+            if isinstance(event, Log):
+                logs.append(event.message)
+            elif isinstance(event, Done):
+                status = (
+                    schema.Status.FAILED if event.error else schema.Status.SUCCEEDED
+                )
+    except Exception:
+        logs.append(traceback.format_exc())
+        status = schema.Status.FAILED
+
+    if status is None:
+        logs.append("Error: did not receive 'done' event from setup!")
+        status = schema.Status.FAILED
+
+    completed_at = datetime.now(tz=timezone.utc)
+
+    return {
+        "logs": "".join(logs),
+        "status": status,
+        "started_at": started_at,
+        "completed_at": completed_at,
+    }
 
 
 def predict(

pkg/predict/predictor.go
@@ -17,6 +17,10 @@ import (
 
 type status string
 
+type HealthcheckResponse struct {
+	Status string `json:"status"`
+}
+
 type Request struct {
 	// TODO: could this be Inputs?
 	Input map[string]string `json:"input"`
@@ -82,7 +86,7 @@ func (p *Predictor) Start(logsWriter io.Writer) error {
 }
 
 func (p *Predictor) waitForContainerReady() error {
-	url := fmt.Sprintf("http://localhost:%d/", p.port)
+	url := fmt.Sprintf("http://localhost:%d/health-check", p.port)
 
 	start := time.Now()
 	for {
@@ -108,7 +112,21 @@ func (p *Predictor) waitForContainerReady() error {
 		if resp.StatusCode != http.StatusOK {
 			continue
 		}
-		return nil
+		healthcheck := &HealthcheckResponse{}
+		if err := json.NewDecoder(resp.Body).Decode(healthcheck); err != nil {
+			return fmt.Errorf("Container healthcheck returned invalid response: %w", err)
+		}
+		// These status values are defined in python/cog/server/http.py
+		switch healthcheck.Status {
+		case "STARTING":
+			continue
+		case "SETUP_FAILED":
+			return fmt.Errorf("Model setup failed")
+		case "READY":
+			return nil
+		default:
+			return fmt.Errorf("Container healthcheck returned unexpected status: %s", healthcheck.Status)
+		}
 	}
 }
 

python/tests/server/conftest.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import time
 from typing import Any, Dict, Optional
 
 from attrs import define
@@ -45,6 +46,15 @@ def make_client(fixture_name: str, upload_url: Optional[str] = None):
     return TestClient(app)
 
 
+def wait_for_setup(client: TestClient):
+    while True:
+        resp = client.get("/health-check")
+        data = resp.json()
+        if data["status"] != "STARTING":
+            break
+        time.sleep(0.01)
+
+
 @pytest.fixture
 def client(request):
     fixture_name = request.param.predictor_fixture
@@ -52,4 +62,5 @@ def client(request):
 
     # Use context manager to trigger setup/shutdown events.
     with make_client(fixture_name=fixture_name, **options) as c:
+        wait_for_setup(c)
         yield c

python/tests/server/test_runner.py
@@ -33,20 +33,28 @@ def runner():
         predictor_ref=_fixture_path("sleep"), shutdown_event=threading.Event()
     )
     try:
+        runner.setup().get(5)
         yield runner
     finally:
         runner.shutdown()
 
 
-def test_prediction_runner_setup(runner):
-    status, logs = runner.setup()
+def test_prediction_runner_setup():
+    runner = PredictionRunner(
+        predictor_ref=_fixture_path("sleep"), shutdown_event=threading.Event()
+    )
+    try:
+        result = runner.setup().get(5)
 
-    assert status == Status.SUCCEEDED
-    assert logs == ""
+        assert result["status"] == Status.SUCCEEDED
+        assert result["logs"] == ""
+        assert isinstance(result["started_at"], datetime)
+        assert isinstance(result["completed_at"], datetime)
+    finally:
+        runner.shutdown()
 
 
 def test_prediction_runner(runner):
-    runner.setup()
     request = PredictionRequest(input={"sleep": 0.1})
     _, async_result = runner.predict(request)
     response = async_result.get(timeout=1)
@@ -59,17 +67,19 @@ def test_prediction_runner(runner):
 
 
 def test_prediction_runner_called_while_busy(runner):
-    runner.setup()
     request = PredictionRequest(input={"sleep": 0.1})
-    runner.predict(request)
+    _, async_result = runner.predict(request)
 
     assert runner.is_busy()
     with pytest.raises(RunnerBusyError):
         runner.predict(request)
 
+    # Call .get() to ensure that the first prediction is scheduled before we
+    # attempt to shut down the runner.
+    async_result.get()
+
 
 def test_prediction_runner_called_while_busy_idempotent(runner):
-    runner.setup()
     request = PredictionRequest(id="abcd1234", input={"sleep": 0.1})
 
     runner.predict(request)
@@ -83,7 +93,6 @@ def test_prediction_runner_called_while_busy_idempotent(runner):
 
 
 def test_prediction_runner_called_while_busy_idempotent_wrong_id(runner):
-    runner.setup()
     request1 = PredictionRequest(id="abcd1234", input={"sleep": 0.1})
     request2 = PredictionRequest(id="5678efgh", input={"sleep": 0.1})
 
@@ -98,7 +107,6 @@ def test_prediction_runner_called_while_busy_idempotent_wrong_id(runner):
 
 
 def test_prediction_runner_cancel(runner):
-    runner.setup()
     request = PredictionRequest(input={"sleep": 0.5})
     _, async_result = runner.predict(request)
 
@@ -114,7 +122,6 @@ def test_prediction_runner_cancel(runner):
 
 
 def test_prediction_runner_cancel_matching_id(runner):
-    runner.setup()
     request = PredictionRequest(id="abcd1234", input={"sleep": 0.5})
     _, async_result = runner.predict(request)
 
@@ -126,7 +133,6 @@ def test_prediction_runner_cancel_matching_id(runner):
 
 
 def test_prediction_runner_cancel_by_mismatched_id(runner):
-    runner.setup()
     request = PredictionRequest(id="abcd1234", input={"sleep": 0.5})
     _, async_result = runner.predict(request)
 

commit ab7666fd140d11d5f3987d15ba4e26f78f2014da

Only mark container as ready after successful setup
294a919 accidentally changed the behaviour of container readiness
checks: model containers started marking themselves as "ready" before
the model setup had successfully completed.

This is a problem, because it means a deployment rollout will
potentially proceed before a new container is actually ready to service
incoming predictions.

This change moves our use of ProbeHelper into Runner so that it's only
called once a model has successfully completed setup.

Signed-off-by: Nick Stenning <nick@whiteink.com>

python/cog/server/http.py
@@ -30,7 +30,6 @@ from ..predictor import (
     load_config,
     load_predictor_from_ref,
 )
-from .probes import ProbeHelper
 from .runner import PredictionRunner, RunnerBusyError, UnknownPredictionError
 
 log = structlog.get_logger("cog.server.http")
@@ -82,9 +81,6 @@ def create_app(
         RunVar("_default_thread_limiter").set(CapacityLimiter(threads))  # type: ignore
 
         app.state.setup_result = runner.setup()
-
-        probes = ProbeHelper()
-        probes.ready()
 
     @app.on_event("shutdown")
     def shutdown() -> None:

python/cog/server/runner.py
@@ -16,6 +16,7 @@ from .. import types
 from ..files import put_file_to_signed_endpoint
 from ..json import upload_files
 from .eventtypes import Done, Heartbeat, Log, PredictionOutput, PredictionOutputType
+from .probes import ProbeHelper
 from .webhook import webhook_caller_filtered
 from .worker import Worker
 
@@ -303,6 +304,11 @@ def setup(*, worker: Worker):
         status = schema.Status.FAILED
 
     completed_at = datetime.now(tz=timezone.utc)
+
+    # Only if setup succeeded, mark the container as "ready".
+    if status == schema.Status.SUCCEEDED:
+        probes = ProbeHelper()
+        probes.ready()
 
     return {
         "logs": "".join(logs),